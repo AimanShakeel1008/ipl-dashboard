223aa1452e33e1c13bbb5bfdadef7cc33d6105f0
aimanshakeel1008@gmail.com <aiman@AIMAN-PC>
2022-06-03 17:34:54 +0530

    committing again

--- bin/src/frontend/src/pages/TeamPage.js
+++ bin/src/frontend/src/pages/TeamPage.js
@@ -1,18 +1,7 @@
-import {React,useEffect} from 'react';
+import {React} from 'react';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 export const TeamPage=()=>{
-
-    useEffect(()=>{
-        const fetchMatches=async ()=>{
-            const response=await fetch('http://localhost:8080/team/Mumbai%20Indians');
-            const data=await response.json();
-            console.log(data);
-        }
-
-        fetchMatches();
-    })
-
     return(
         <div className='TeamPage'>
             <h1>Team Name</h1>
