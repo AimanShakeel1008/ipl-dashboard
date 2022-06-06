fa401e67adf5f8ab40849adb1f08f4b049d47b8a
aimanshakeel1008@gmail.com <aiman@AIMAN-PC>
2022-06-06 20:09:26 +0530

    called match API in MatchPage component

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,31 @@
-import {React} from 'react';
+import {React,useState,useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+
+
+
 export const MatchPage=()=>{
+
+    const[matches,setMatches]=useState([]);
+    const {teamName,year}=useParams();
+
+    useEffect(()=>{
+        const fetchMatches=async ()=>{
+            const response=await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data=await response.json();
+            console.log(data);
+            setMatches(data);
+        }
+
+        fetchMatches();
+    },[teamName])
 
     return(
         <div className='MatchPage'>
             <h1>MatchPage</h1>
+            {
+                matches.map(match=><MatchDetailCard teamName={teamName} key={match.id} match={match}/>)
+            }
         </div>
     )
 }
